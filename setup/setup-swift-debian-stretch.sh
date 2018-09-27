1d096756a20de19e3e5916ceec272e63629e518e
Thomas Günzel <thomasguenzel@me.com>
2018-09-27 16:27:29 +0200

    improve scripts

--- setup/setup-swift-debian-stretch.sh
+++ setup/setup-swift-debian-stretch.sh
@@ -2,11 +2,6 @@ SWIFT_PACKAGE="swift-4.2-RELEASE-ubuntu18.04"
 SWIFT_URL="https://swift.org/builds/swift-4.2-release/ubuntu1804/swift-4.2-RELEASE/swift-4.2-RELEASE-ubuntu18.04.tar.gz"
 
 PREV_WD="$(pwd)"
-pathadd() {
-	if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-		export PATH="$1:${PATH}"
-	fi
-}
 
 # Install Necessary Swift Components
 apt-get update
@@ -16,7 +11,7 @@ apt-get update
 apt-get -y install clang-6.0 libpython2.7 libcurl3 libicu-dev
 
 # Install unstable libicu60
-if [-f /etc/apt/preferences.d/swift-unstable-libicu ]
+if [ -f /etc/apt/preferences.d/swift-unstable-libicu ]
 then
 	echo "apt pinning already installed. skipping"
 else
@@ -29,6 +24,7 @@ Package: *
 Pin: release a=unstable
 Pin-Priority: 600
 EOT
+	echo "apt pinning added"
 fi
 
 apt-get update
@@ -43,12 +39,10 @@ cd .swift
 # Download and Unpack
 wget $SWIFT_URL
 tar xzf "${SWIFT_PACKAGE}.tar.gz"
-if [-f /root/.swift/current ]
+if [ -f /root/.swift/current ]
 then
 	rm /root/.swift/current
 	echo "removed old symlink"
 fi
 ln -s /root/.swift/"${SWIFT_PACKAGE}" /root/.swift/current
 
-pathadd /root/.swift/current/usr/bin
-pathadd /usr/lib/llvm-6.0/bin
---
+++ setup/setup-swift-paths.sh
@@ -0,0 +1,2 @@
+export PATH=/root/.swift/current/usr/bin:"${PATH}"
+export PATH=/usr/lib/llvm-6.0/bin:"${PATH}"
